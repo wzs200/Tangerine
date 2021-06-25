a48c4222cc05d32690338a724f0b0a9784feb0f0
wzs200 <Heckfyghbdtn1122>
2021-06-25 19:37:56 +0300

    init

---
+++ mainapp/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+# Create your models here.
+# Category
+# Product
+# CartProduct
+# Cart
+# Order
+# Customer
+# Spesification
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+    # objects = CategoryManager()
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('category_detail', kwargs={'slug': self.slug})
+
+
+class Product(models.Model):
+
+    # class Meta:
+    #     abstract = True
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    description = models.TextField(verbose_name='Описание', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.title
+
+    # def get_model_name(self):
+    #     return self.__class__.__name__.lower()
+
+class CartProduct(models.Model):
+
+    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+
+    def __str__(self):
+        return "Продукт: {} (для корзины)".format(self.product.title)
+
+    # def save(self, *args, **kwargs):
+    #     self.final_price = self.qty * self.content_object.price
+    #     super().save(*args, **kwargs)
+
